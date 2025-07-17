2e94dac2e0028b6106678f1511065aefaa5c583b
NaveedAfraz <naveedafraz123@gmail.com>
2025-07-17 23:51:01 +0530

    added sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,37 @@
- export default function HomePage() {
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+import { AppSidebarClient } from "./_AppSidebarClient";
+
+export default function HomePage() {
   return (
-    <h1>HomePage</h1>
-  )
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">Jobs Search</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter className="flex-col">
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>Logout</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">asdfasdfasdf</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
- 
